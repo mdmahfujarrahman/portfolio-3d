09d80b5e4a5d3780397118d5b88ee2520682dbef
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2023-04-01 10:21:59 +0600

    eslist added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    transpilePackages: ["@react-three/drei", "@react-three/fiber", "three"],
+    // transpilePackages: ["@react-three/drei", "@react-three/fiber", "three"],
 };
 
 module.exports = nextConfig;
