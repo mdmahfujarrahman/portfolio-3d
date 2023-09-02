163055baa50c63c296ced50314412e471f689bea
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2023-09-02 10:02:56 +0600

    facebook chatbox added

---
+++ components/FacebookPlagIn/FacebookPlagIn.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+
+const FacebookPlagIn = () => {
+    return (
+        <>
+            <Head>
+               
+            </Head>
+
+            
+        </>
+    );
+};
+
+export default FacebookPlagIn;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import FacebookPlagIn from "@/components/FacebookPlagIn/FacebookPlagIn";
 import {
     About,
     Banner,
@@ -9,6 +10,7 @@ import {
     Contact,
 } from "@/components/constants";
 import Head from "next/head";
+import { useEffect, useRef } from "react";
 import { Toaster } from "react-hot-toast";
 
 export const metadata = {
@@ -18,11 +20,60 @@ export const metadata = {
 };
 
 const Home = () => {
+    const messengerRef = useRef(null);
+
+    useEffect(() => {
+        if (messengerRef.current) {
+            messengerRef.current.setAttribute(
+                "attribution_version",
+                "biz_inbox"
+            );
+            messengerRef.current.setAttribute("attribution", "install_email");
+            messengerRef.current.setAttribute("page_id", "106645127396616");
+            window.fbAsyncInit = function () {
+                window.FB.init({
+                    xfbml: true,
+                    version: "v16.0",
+                });
+            };
+
+            (function (d, s, id) {
+                var js,
+                    fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src =
+                    "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            })(document, "script", "facebook-jssdk");
+        }
+    }, []);
     return (
         <div className="relative z-0 bg-primary">
             <Head>
                 <title>{metadata.title}</title>
                 <meta name="description" content={metadata.description} />
+                {/* <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                                (function(d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0];
+                                    if (d.getElementById(id)) return;
+                                    js = d.createElement(s); js.id = id;
+                                    js.src = 'https://connect.facebook.net/en_US/sdk.js';
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }(document, 'script', 'facebook-jssdk'));
+
+                                window.fbAsyncInit = function() {
+                                    FB.init({
+                                    xfbml: true,
+                                    version: 'v17.0',
+                                    });
+                                };
+                                `,
+                    }}
+                /> */}
             </Head>
             <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
                 <Navbar />
@@ -37,6 +88,9 @@ const Home = () => {
                 <Contact />
                 {/* <StarsCanvas /> */}
             </div>
+
+            <div id="fb-root" />
+            <div className="fb-customerchat" ref={messengerRef} />
             <Toaster position="top-right" reverseOrder={false} />
         </div>
     );
