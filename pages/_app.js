559666ead7b890dc3fe859037af2c3ac98edeae0
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2024-01-30 09:11:48 +0600

    analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
-
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <SpeedInsights />
+            <Analytics />
+        </>
+    );
 }
